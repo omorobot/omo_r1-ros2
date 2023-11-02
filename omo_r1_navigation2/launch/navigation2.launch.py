82a440a694a1344c35d249cec84db4fd7dcf5f50
t-shaped-person <kucira00@gmail.com>
2023-11-02 19:31:58 +0900

    20231102

--- omo_r1_navigation2/launch/navigation2.launch.py
+++ omo_r1_navigation2/launch/navigation2.launch.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2019 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Author: Bishop Pearson
-
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
@@ -36,13 +19,12 @@ def generate_launch_description():
             'map',
             'turtlebot3_world.yaml'))
 
-    param_file_name = 'omo_r1.yaml'
     param_dir = LaunchConfiguration(
         'params_file',
         default=os.path.join(
             get_package_share_directory('omo_r1_navigation2'),
             'param',
-            param_file_name))
+            'omo_r1.yaml'))
 
     nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
 
