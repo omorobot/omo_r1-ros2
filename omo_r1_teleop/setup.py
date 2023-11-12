5b3ab11f0873345ff683ee0834762e0b17e062e8
t-shaped-person <kucira00@gmail.com>
2023-11-12 12:03:30 +0900

    20231112

--- omo_r1_teleop/setup.py
+++ omo_r1_teleop/setup.py
@@ -1,44 +1,29 @@
-import os
-from glob import glob
-from setuptools import find_packages
-from setuptools import setup
+import os # add
+from glob import glob # add
+from setuptools import find_packages, setup
 
 package_name = 'omo_r1_teleop'
 
 setup(
     name=package_name,
-    version='2.1.2',
-    packages=find_packages(exclude=[]),
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'param'), glob('param/*.yaml')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')), # add
     ],
-    install_requires=[
-        'setuptools',
-    ],
+    install_requires=['setuptools'],
     zip_safe=True,
-    author='Bishop Pearson',
-    author_email='bishopearson@gmail.com',
-    maintainer='Bishop Pearson',
-    maintainer_email='bishopearson@gmail.com',
-    keywords=['ROS'],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
-    ],
-    description=(
-        'Teleoperation node using keyboard or joystick for the OMORobot R1 Mini'
-    ),
+    maintainer='Dr.K',
+    maintainer_email='kucira00@gmail.com',
+    description='Teleop package',
     license='Apache License, Version 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'teleop_keyboard = omo_r1_teleop.script.teleop_keyboard:main',
-            'teleop_joy = omo_r1_teleop.script.teleop_joy:main' 
+            'teleop_keyboard = omo_r1_teleop.teleop_keyboard:main'
         ],
     },
 )
