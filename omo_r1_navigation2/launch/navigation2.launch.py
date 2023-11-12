ca17955a02fbbf752459a7903dcbddcd72f6b954
t-shaped-person <kucira00@gmail.com>
2023-11-12 20:42:30 +0900

    20231112

--- omo_r1_navigation2/launch/navigation2.launch.py
+++ omo_r1_navigation2/launch/navigation2.launch.py
@@ -1,54 +1,32 @@
 #!/usr/bin/env python3
-
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
+    navigation2_dir = get_package_share_directory('omo_r1_navigation2')
+    nav2_launch_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+
+    map = LaunchConfiguration('map', default=os.path.join(navigation2_dir, 'map', 'turtle_world.yaml'))
+    params_file = LaunchConfiguration('params_file', default=os.path.join(navigation2_dir, 'param', 'omo_r1.yaml'))
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-    map_dir = LaunchConfiguration(
-        'map',
-        default=os.path.join(
-            get_package_share_directory('omo_r1_navigation2'),
-            'map',
-            'turtlebot3_world.yaml'))
 
-    param_dir = LaunchConfiguration(
-        'params_file',
-        default=os.path.join(
-            get_package_share_directory('omo_r1_navigation2'),
-            'param',
-            'omo_r1.yaml'))
+    map_arg = DeclareLaunchArgument('map', default_value=map)
+    params_file_arg = DeclareLaunchArgument('params_file', default_value=params_file)
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value=use_sim_time)
 
-    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+    navigation2_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([nav2_launch_dir, '/bringup_launch.py']),
+        launch_arguments={'map': map, 'params_file': params_file, 'use_sim_time': use_sim_time}.items()
+    )
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'map',
-            default_value=map_dir,
-            description='Full path to map file to load'),
+    ld = LaunchDescription()
+    ld.add_action(map_arg)
+    ld.add_action(params_file_arg)
+    ld.add_action(use_sim_time_arg)
+    ld.add_action(navigation2_node)
 
-        DeclareLaunchArgument(
-            'params_file',
-            default_value=param_dir,
-            description='Full path to param file to load'),
-
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
-            launch_arguments={
-                'map': map_dir,
-                'use_sim_time': use_sim_time,
-                'params_file': param_dir}.items(),
-        )
-    ])
+    return ld
--- omo_r1_navigation2/launch/navigation2_rviz.launch.py
+++ omo_r1_navigation2/launch/navigation2_rviz.launch.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
-
 import os
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    nav2_dir = get_package_share_directory('nav2_bringup')
+
+    rviz_file = LaunchConfiguration('rviz_file', default=os.path.join(nav2_dir, 'rviz', 'nav2_default_view.rviz'))
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
-    rviz_config_dir = os.path.join(
-        get_package_share_directory('nav2_bringup'),
-        'rviz',
-        'nav2_default_view.rviz')
+    rviz_file_arg = DeclareLaunchArgument('rviz_file', default_value=rviz_file)
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value=use_sim_time)
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_file],
+        parameters=[{'use_sim_time': use_sim_time}]
+    )
 
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
-    ])
+    ld = LaunchDescription()
+    ld.add_action(rviz_file_arg)
+    ld.add_action(use_sim_time_arg)
+    ld.add_action(rviz2_node)
+
+    return ld
