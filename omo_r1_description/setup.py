4050c3c8ed3594a1a2317372185b250f789574fc
t-shaped-person <kucira00@gmail.com>
2023-11-19 14:08:02 +0900

    20231119

---
+++ omo_r1_description/setup.py
@@ -0,0 +1,33 @@
+import os # add
+from glob import glob # add
+from setuptools import setup
+
+package_name = 'omo_r1_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')), # add
+        (os.path.join('share', package_name, 'meshes/bases'), glob('meshes/bases/*')), # add
+        (os.path.join('share', package_name, 'meshes/sensors'), glob('meshes/sensors/*')), # add
+        (os.path.join('share', package_name, 'meshes/wheels'), glob('meshes/wheels/*')), # add
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')), # add
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')), # add
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Dr.K',
+    maintainer_email='kucira00@gmail.com',
+    description='Description package',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
