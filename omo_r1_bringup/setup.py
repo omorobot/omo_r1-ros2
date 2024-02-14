0a84199781e90afc221ebc39d4902c1b0a3211e0
t-shaped-person <kucira00@gmail.com>
2024-02-14 21:22:51 +0900

    20240214

--- omo_r1_bringup/setup.py
+++ omo_r1_bringup/setup.py
@@ -1,44 +1,29 @@
-import os
-from glob import glob
-from setuptools import find_packages
-from setuptools import setup
+import os # add
+from glob import glob # add
+from setuptools import find_packages, setup
 
 package_name = 'omo_r1_bringup'
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=find_packages(exclude=[]),
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'param'), glob(os.path.join('param', '*.yaml'))),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')), # add
+        (os.path.join('share', package_name, 'param'), glob('param/*')), # add
     ],
-    install_requires=[
-        'setuptools',
-    ],
+    install_requires=['setuptools'],
     zip_safe=True,
-    author='Bishop Pearson',
-    author_email='bishopearson@gmail.com',
-    maintainer='Bishop Pearson',
-    maintainer_email='bishopearson@gmail.com',
-    keywords=['ROS'],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
-    ],
-    description=(
-        'OMORobot R1 Mini driver node that include iff drive controller, odometry and tf node'
-    ),
+    maintainer='Dr.K',
+    maintainer_email='kucira00@gmail.com',
+    description='Bringup package',
     license='Apache License, Version 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'omo_r1_mcu_node = omo_r1_bringup.scripts.omo_r1_mcu_node:main',
-            'omo_r1_motor_setting_node = omo_r1_bringup.scripts.omo_r1_motor_setting_node:main'
         ],
     },
 )
